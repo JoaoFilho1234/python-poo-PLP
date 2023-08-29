078035f7db68088829284e030040124bf5be0e09
JoaoFilho1234 <joaofilho.jbf28@gmail.com>
2023-08-29 16:40:50 -0300

    Interrupção e Tratamento de exceção

--- NOTAS PYTHON/Menu.py
+++ NOTAS PYTHON/Menu.py
@@ -1,14 +1,37 @@
 from RepositorioAluno import RepositorioAluno
-    
-numero = int(input("__MENU__\n 1. Adicionar Aluno\n 2. Verificar Alunos\n 0. Sair\n"))
-aluno = RepositorioAluno()
-while numero!=0:
-    
-    if numero == 1:    
-        
-        aluno.nome((input("Digite o nome do Aluno: ")))
-        aluno.notas((float(input("Digite a nota do Aluno: "))))
 
-    if numero == 2:
-        aluno.mostraAlunos()
-    numero = int(input("__MENU__\n 1. Adicionar Aluno\n 2. Verificar Alunos\n 0. Sair\n"))
+
+def possui_numeros(texto):
+    return any(caractere.isdigit() for caractere in texto)
+
+
+try:
+    aluno = RepositorioAluno()
+
+    while True:
+        try:
+            numero = int(input("__MENU__\n 1. Adicionar Aluno\n 2. Verificar Alunos\n 0. Sair\n"))
+
+            if numero == 1:
+                nome_aluno = input("Digite o nome do Aluno: ")
+                if possui_numeros(nome_aluno):
+                    print("Erro: O nome do aluno não deve conter números.")
+                    continue
+                nota_aluno = float(input("Digite a nota do Aluno: "))
+                aluno.nome(nome_aluno)
+                aluno.notas(nota_aluno)
+
+            elif numero == 2:
+                aluno.mostraAlunos()
+
+            elif numero == 0:
+                print("Programa interrompido! Saindo do programa...")
+                break
+            else:
+                print("Opção inválida. Escolha uma opção válida.")
+
+        except ValueError:
+            print("Erro: Digite um valor numérico válido.")
+
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
